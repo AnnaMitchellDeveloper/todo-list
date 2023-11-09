da913a6cf66ed6d7272eb934c839e9fb116ff4fc
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-11-09 12:53:30 -0500

    Added delete, check, and filter buttons and  functionality in JS

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 const todoInput = document.querySelector(".input");
 const todoButton = document.querySelector(".btn");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".task-filter");
 
 //Event Listeners
-todoButton.addEventListener("click", addTask)
+todoButton.addEventListener("click", addTask);
+todoList.addEventListener("click", deleteOrCheck);
+filterOption.addEventListener("click", filterTasks);
 
 //Functions
 function addTask(e){
@@ -35,4 +38,48 @@ function addTask(e){
   todoList.appendChild(todoDiv);
   //Clear Todo Input value
   todoInput.value = "";
+}
+
+function deleteOrCheck(e){
+  const item = e.target;
+  //Delete Task
+  if(item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    //Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function(){
+      todo.remove();
+    })
+    
+  }
+  //Check Mark
+  if(item.classList[0] === "mark-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+  }
+}
+
+function filterTasks(e){
+  const todos = todoList.childNodes;
+  todos.forEach(function(todo){
+    switch(e.target.value){
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if(todo.classList.contains("completed")){
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if(!todo.classList.contains("completed")){
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  })
 }
